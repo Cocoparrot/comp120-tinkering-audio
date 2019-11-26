30bc73dda4bd9e39bdc223d211b1c7262f162ac8
sam hendrickx <sh233616@falmouth.ac.uk>
2019-11-26 10:00:36 +0000

    recommit because something went wrong

---
+++ AudioTinkering_Contract!/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller; //Variable to initialise the controller we use for the movement.
+
+    public float speed = 12f; //float for the speed at which the controller moves
+   
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+         *inistialize the X and Z axis movement to keep track of which way we move. 
+         * Can populate the values with the charactercontroller functions of input.GetAxis and the according direction axis
+         */
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //Calculate the movement multiply the transform so the movement is always in according to the way the character is looking, rather than the actual position in Unity
+        Vector3 move = transform.right * x + transform.forward * z;
+           
+        //Move the character in the direction calculated above and adding the speed of movement and making it relative to the Time so the framerate does not influence it
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
---
+++ AudioTinkering_Contract!/Assets/Scripts/Steps.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steps : MonoBehaviour
+{
+
+
+    public float stepCoolDown; //initialise the stepCooldown variable we will use to keep track of the actual steps made
+    public float stepRate = 0.5f; // 
+    public AudioSource stepSound; //Reference to the audiosource which we want to play.
+
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        //make the timing negative so we can reset to 0.
+        stepCoolDown -= Time.deltaTime;
+
+        /*If case to make sure the character is actually moving before playing the sound.
+        *We use the cooldown to make sure the sounds are not overlapping each other.
+        * We use 2 random values voor the pitch and volume so every step sounds different
+        * We can use the input.GetAxis function to track movement since we have a charactercontroller on the object we are moving easier than to calculate every step
+        */
+
+        if ((Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f) && stepCoolDown < 0f)
+        {
+            stepSound.pitch = Random.Range(0.6f, 1f);
+            stepSound.volume = Random.Range(0.6f, 1.1f);
+            stepSound.Play();
+            stepCoolDown = stepRate; //reset the timer everytime a new step is made
+        }
+    }
+}
